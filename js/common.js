c07457c91948bd17d6c16cb5fc1862c7c16f86d6
Zolotuhin Iliya <argo0003@ya.ru>
2018-06-02 16:10:17 +0300

    add project

---
+++ js/common.js
@@ -0,0 +1,36 @@
+$(function() {
+
+ $('.logo-litera').each(function() {
+	var ths = $(this);
+	ths.html(ths.html().replace('the', '<span>the</span>'));
+ });
+
+ $('.search').click(function(){
+	$('.search-field').stop().slideToggle();
+	$('.search-field input[type=text]').focus();
+ });
+
+ $(document).keyup(function(e) {
+	 if (e.keyCode == 27) {
+		$('.search-field').slideUp();
+	 }
+ }).click(function() {
+	$('.search-field').slideUp();
+ });
+ $('.search-wrap').click(function(e) {
+	 e.stopPropagation();
+ });
+
+ $('.top-line').after('<div class="mobile-menu d-lg-none">');
+ $('.top-menu').clone().appendTo('.mobile-menu');
+ $('.mobile-menu-button').click(function() {
+	$('.mobile-menu').stop().slideToggle();
+ });
+
+ $("body").prognroll({
+	height: 3,
+	color: "#DF3167",
+	custom: false
+});
+
+});
